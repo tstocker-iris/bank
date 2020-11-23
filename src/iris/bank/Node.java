e03d2f954c5e6ba9033bb1aca4a0b80b8cc9ed1f
Thomas STOCKER <thomas.stocker92@gmail.com>
2020-11-23 11:41:01 +0100

    Ajout de commentaire

--- src/iris/bank/Node.java
+++ src/iris/bank/Node.java
@@ -73,22 +73,33 @@ public class Node {
 
     public Node delete(String pNom) {
         if (this.etiquette != null) {
+            // Parcours horizontal de l'arbre pour trouver le noeud a supprimer
             if (this.etiquette.compareTo(pNom) < 0) {
                 this.filsDroit = this.filsDroit.delete(pNom);
             } else if (this.etiquette.compareTo(pNom) > 0) {
                 this.filsGauche = this.filsGauche.delete(pNom);
             } else {
-                if (this.filsGauche == null) {
+                // On a trouvé le noeud à supprimer
+                // On vérifie s'il n'a pas de fils gauche et un fils droit
+                if (this.filsGauche == null && this.filsDroit != null) {
+                    // On retourne le fils droit
                     return this.filsDroit;
-                } else if (this.filsDroit == null) {
+                // On vérifie s'il n'a pas de fils droit et un fils gauche
+                } else if (this.filsDroit == null && this.filsGauche != null) {
+                    // On retourne le fils gauche
                     return this.filsGauche;
                 }
 
+                // Sinon c'est qu'il a deux fils, on part sur le fils droit (mais on pourrait partir sur le gauche)
+                // On cherche le successeur au fils droit
                 Node successor = this.filsDroit.successor();
+                // S'il n'est pas vide
                 if (successor.etiquette != null && successor.etiquette.name != this.etiquette.name) {
+                    // On affecte le successeur au noeud courant (donc son etiquette et ses fils droit)
                     this.etiquette = successor.etiquette;
                     this.filsDroit = this.filsDroit.delete(successor.etiquette.name);
                 } else {
+                    // Sinon c'est qu'il n'a pas de fils, on supprime juste sa valeur
                     this.etiquette = null;
                 }
             }
